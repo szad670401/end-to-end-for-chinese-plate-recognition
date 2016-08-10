17bb2d99e87079f7efa61a846974f6751a68a1b4
Jackyu <455501914@qq.com>
2016-08-10 16:10:40 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,8 +8,7 @@ import numpy as np
 import cv2, random
 import os
 from io import BytesIO
-from captcha.image import ImageCaptcha
-from cnn_ocr import gen_rand, gen_sample
+from train import gen_rand, gen_sample
 chars = ["京", "沪", "津", "渝", "冀", "晋", "蒙", "辽", "吉", "黑", "苏", "浙", "皖", "闽", "赣", "鲁", "豫", "鄂", "湘", "粤", "桂",
              "琼", "川", "贵", "云", "藏", "陕", "甘", "青", "宁", "新", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A",
              "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
